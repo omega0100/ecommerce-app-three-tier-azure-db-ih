c21f95668567cb1cafd8b3708a0e22c967a6e2ac
Abdullah010 <166231259+omega0100@users.noreply.github.com>
2025-09-18 23:24:23 +0300

    Update install-app.sh
    2

--- install-app.sh
+++ install-app.sh
@@ -5,20 +5,31 @@ set -e
 sudo apt-get update -y
 sudo apt-get upgrade -y
 
-# تثبيت Docker و Docker Compose
-sudo apt-get install -y docker.io docker-compose-plugin git curl
+# إزالة أي تعارض مع containerd
+sudo apt-get remove -y containerd.io || true
+
+# تثبيت Docker بالطريقة الرسمية
+if ! command -v docker &> /dev/null
+then
+  curl -fsSL https://get.docker.com -o get-docker.sh
+  sudo sh get-docker.sh
+fi
+
+# تثبيت docker-compose (plugin)
+sudo apt-get install -y docker-compose-plugin git curl
 
 # تفعيل docker
 sudo systemctl enable docker
 sudo systemctl start docker
 
 # تنزيل المشروع (لو ما هو موجود)
-if [ ! -d "/home/azureuser/ecommerce-app-three-tier-azure-db-ih" ]; then
+APP_DIR="/home/azureuser/ecommerce-app-three-tier-azure-db-ih"
+if [ ! -d "$APP_DIR" ]; then
   cd /home/azureuser
-  git https://github.com/omega0100/ecommerce-app-three-tier-azure-db-ih.git
+  git clone https://github.com/omega0100/ecommerce-app-three-tier-azure-db-ih.git
 fi
 
-cd /home/azureuser/ecommerce-app-three-tier-azure-db-ih
+cd $APP_DIR
 
 # تشغيل docker compose
 sudo docker compose up -d --build
